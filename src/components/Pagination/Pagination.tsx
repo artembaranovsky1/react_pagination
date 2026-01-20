9d4141115fe1369da5c3d64f46334d0e564f0672
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-01-20 13:42:47 +0200

    solutioin

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,71 @@
-export const Pagination = () => {};
+// import { getNumbers } from '../../utils';
+import React from 'react';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / +perPage);
+  const arrNumber = [];
+
+  for (let i = 0; i < totalPages; i++) {
+    arrNumber.push(i + 1);
+  }
+
+  return (
+    <ul className="pagination">
+      <li className={currentPage !== 1 ? 'page-item' : 'page-item disabled'}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={currentPage === 1}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
+          «
+        </a>
+      </li>
+
+      {arrNumber.map(page => (
+        <li
+          className={page === currentPage ? 'page-item active' : 'page-item'}
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+
+      <li
+        className={
+          currentPage !== totalPages ? 'page-item' : 'page-item disabled'
+        }
+      >
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={currentPage === totalPages}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
